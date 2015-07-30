eebb472a739819cf9ddef36851da459838d1c2ba
TGA-Student <tga.student@hackreactor.com>
2015-07-29 18:53:22 -0700

    grunt file ready for commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,13 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      options:{
+        seperator: ';',
+      },
+      dist: {
+        src: ['public/client/*.js'],
+        dest: 'public/dist/built.js'
+      }
     },
 
     mochaTest: {
@@ -20,22 +27,48 @@ module.exports = function(grunt) {
       }
     },
 
-    uglify: {
-    },
-
-    jshint: {
+    uglify:  {
+       dynamic_mappings: {
+      // Grunt will search for "**/*.js" under "lib/" when the "uglify" task
+      // runs and build the appropriate src-dest file mappings then, so you
+      // don't need to update the Gruntfile when files are added or removed.
       files: [
-        // Add filespec list here
+        {
+          expand: true,     // Enable dynamic expansion.
+          cwd: 'public/client',      // Src matches are relative to this path.
+          src: ['**/*.js'], // Actual pattern(s) to match.
+          dest: 'public/build/',   // Destination path prefix.
+          ext: '.min.js',   // Dest filepaths will have this extension.
+          extDot: 'first'   // Extensions in filenames begin after the first dot
+        },
+        {
+          expand: true,     // Enable dynamic expansion.
+          cwd: 'public/lib',      // Src matches are relative to this path.
+          src: ['**/*.js'], // Actual pattern(s) to match.
+          dest: 'public/build/',   // Destination path prefix.
+          ext: '.min.js',   // Dest filepaths will have this extension.
+          extDot: 'first'   // Extensions in filenames begin after the first dot
+        }
       ],
+    },
       options: {
-        force: 'true',
-        jshintrc: '.jshintrc',
-        ignores: [
-          'public/lib/**/*.js',
-          'public/dist/**/*.js'
-        ]
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       }
     },
+
+      jshint: {
+        all: ['app/**/*.js', 'lib/**/*.js', 'public/**/*.js', 'test/**/*.js', '*.js'],
+        options: {
+          force: 'true',
+          jshintrc: '.jshintrc',
+          ignores: [
+          'public/lib/**/*.js',
+          'public/dist/**/*.js',
+          'public/build/**/*.js'
+          ]
+        }
+      },
+
 
     cssmin: {
     },
@@ -43,12 +76,12 @@ module.exports = function(grunt) {
     watch: {
       scripts: {
         files: [
-          'public/client/**/*.js',
-          'public/lib/**/*.js',
+        'public/client/**/*.js',
+        'public/lib/**/*.js',
         ],
         tasks: [
-          'concat',
-          'uglify'
+        'concat',
+        'uglify'
         ]
       },
       css: {
@@ -60,25 +93,26 @@ module.exports = function(grunt) {
     shell: {
       prodServer: {
       }
-    },
+    }
+
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-mocha-test');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-nodemon');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-mocha-test');
+grunt.loadNpmTasks('grunt-shell');
+grunt.loadNpmTasks('grunt-nodemon');
 
-  grunt.registerTask('server-dev', function (target) {
+grunt.registerTask('server-dev', function (target) {
     // Running nodejs in a different process and displaying output on the main console
     var nodemon = grunt.util.spawn({
-         cmd: 'grunt',
-         grunt: true,
-         args: 'nodemon'
-    });
+     cmd: 'grunt',
+     grunt: true,
+     args: 'nodemon'
+   });
     nodemon.stdout.pipe(process.stdout);
     nodemon.stderr.pipe(process.stderr);
 
@@ -89,12 +123,14 @@ module.exports = function(grunt) {
   // Main grunt tasks
   ////////////////////////////////////////////////////
 
-  grunt.registerTask('test', [
-    'mochaTest'
-  ]);
+  grunt.registerTask('default', ['jshint', 'concat','mochaTest']);
+
+  // grunt.registerTask('test', [
+  //   'mochaTest'
+  //   ]);
 
   grunt.registerTask('build', [
-  ]);
+    ]);
 
   grunt.registerTask('upload', function(n) {
     if(grunt.option('prod')) {
@@ -104,9 +140,7 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask('deploy', [
-    // add your deploy tasks here
-  ]);
+  grunt.registerTask('deploy', ['uglify']);
 
 
 };
